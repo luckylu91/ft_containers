00437de0ef5281c8d9d922dfc9c8b5497af84072
Lucas Zins <zins.lucas@gmail.com>
2021-08-11 11:29:38 +0200

    push

--- vector_iterator.cpp
+++ vector_iterator.cpp
@@ -1,31 +1,91 @@
-#include "vector.hpp"
 #include <iostream>
 
-int main()
-{
-	ft::vector<int> intVect;
-	for (int i = 0; i < 10; i++)
-		intVect.push_back(i);
+#include "vector.hpp"
 
-	std::cout << "Mutable iterator" << std::endl;
-	for (ft::vector<int>::iterator it = intVect.begin(); it != intVect.end(); ++it)
-		std::cout << *it << ", ";
-	std::cout << "END" << std::endl;
+void test_types() {
+  ft::vector<int> intVect;
+  for (int i = 0; i < 10; i++)
+    intVect.push_back(i);
 
-	std::cout << "Mutable reverse iterator" << std::endl;
-	for (ft::vector<int>::reverse_iterator it = intVect.rbegin(); it != intVect.rend(); ++it)
-		std::cout << *it << ", ";
-	std::cout << "END" << std::endl;
+  std::cout << "Mutable iterator" << std::endl;
+  for (ft::vector<int>::iterator it = intVect.begin(); it != intVect.end(); ++it)
+    std::cout << *it << ", ";
+  std::cout << "END" << std::endl;
 
-	ft::vector<int> const cIntVect(intVect);
+  std::cout << "Mutable reverse iterator" << std::endl;
+  for (ft::vector<int>::reverse_iterator it = intVect.rbegin(); it != intVect.rend(); ++it)
+    std::cout << *it << ", ";
+  std::cout << "END" << std::endl;
 
-	std::cout << "Const iterator" << std::endl;
-	for (ft::vector<int>::const_iterator it = cIntVect.begin(); it != cIntVect.end(); ++it)
-		std::cout << *it << ", ";
-	std::cout << "END" << std::endl;
+  ft::vector<int> const cIntVect(intVect);
+  std::cout << "Const iterator" << std::endl;
+  for (ft::vector<int>::const_iterator it = cIntVect.begin(); it != cIntVect.end(); ++it)
+    std::cout << *it << ", ";
+  std::cout << "END" << std::endl;
 
-	std::cout << "Reverse const iterator" << std::endl;
-	for (ft::vector<int>::const_reverse_iterator it = cIntVect.rbegin(); it != cIntVect.rend(); ++it)
-		std::cout << *it << ", ";
-	std::cout << "END" << std::endl;
+  std::cout << "Reverse const iterator" << std::endl;
+  for (ft::vector<int>::const_reverse_iterator it = cIntVect.rbegin(); it != cIntVect.rend(); ++it)
+    std::cout << *it << ", ";
+  std::cout << "END" << std::endl;
+}
+
+// operator*
+// operator-> !
+// operator++
+// operator++(int)
+// operator--
+// operator--(int)
+// operator[]
+// operator+=
+// operator-=
+// operator-
+// operator+
+// operator+
+// operator-
+// operator==
+// operator!=
+// operator<
+// operator>
+// operator<=
+// operator>=
+// Require size >= 5
+template <class InputIterator>
+void test_methods(InputIterator it) {
+  typedef typename InputIterator::difference_type difference_type;
+  typedef typename InputIterator::value_type value_type;
+  typedef typename InputIterator::pointer pointer;
+  typedef typename InputIterator::reference reference;
+
+  std::cout << "*it = " << *it<< std::endl;
+  // operator++
+  std::cout << "++it = " << ++it << ", now *it = " << *it << std::endl;
+  // operator++(int)
+  std::cout << "it++ = " << it++ << ", now *it = " << *it << std::endl;
+  // operator--
+  std::cout << "--it = " << --it << ", now *it = " << *it << std::endl;
+  // operator--
+  std::cout << "it-- = " << it-- << ", now *it = " << *it << std::endl;
+  // operator[]
+  std::cout << "it[0]" << it[0] << ", it[2]" << it[2] << std::endl;
+  // operator+=
+  std::cout << "it += 2" << ", now *it = " << *it << std::endl;
+  // operator-=
+  std::cout << "it -= 1" << ", now *it = " << *it << std::endl;
+  // operator-
+  std::cout << "*(it - 1) = " << *(it - 1) << std::endl;
+  // operator+
+  std::cout << "*(it + 1) = " << *(it + 1) << std::endl;
+  // operator+
+  std::cout << "*(1 + it) = " << *(1 + it) << std::endl;
+  // operator-
+  std::cout << "*(it - (it + 2)) = " << *(it - (it + 2)) << std::endl;
+  // operator==
+  // operator!=
+  // operator<
+  // operator>
+  // operator<=
+  // operator>=
+}
+
+int main() {
 }
